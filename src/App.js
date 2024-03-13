b576b7c0b89388b75d5fbdfa5fa8713470d87715
Susmitha-Medepally <116934841+Susmitha-Medepally@users.noreply.github.com>
2024-03-13 14:54:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React,{useState,useEffect} from 'react'
+import './App.css'
 
+import Sentiment from 'sentiment';
 const App = () => {
-  const [posts, setPosts] = useState([]);
+  const sentiment = new Sentiment();
+  const [inputText,setInputText] = useState("");
+  const [result,setResult] = useState();
 
-  useEffect(() => {
-    // Fetch posts from server
-    const fetchPosts = async () => {
-      try {
-        const response = await axios.get(process.env.REACT_APP_API_URL || 'http://localhost:5000/api/fetch-posts');
-        const postsArray = Array.isArray(response.data) ? response.data : []; // Ensure it's an array
-        // Update state with fetched posts
-        setPosts(postsArray);
-      } catch (error) {
-        console.error('Error fetching posts:', error);
-      }
-    };
-
-    fetchPosts();
-  }, []);
+  useEffect(()=>{
+    const tempResult = sentiment.analyze(inputText);
+    setResult(tempResult)
+    console.log(tempResult);
+  },[inputText])
 
   return (
-    <div>
-      <h1>Social Media Monitor</h1>
-      <iframe src='https://widgets.sociablekit.com/instagram-feed/iframe/25379947' frameborder='0' width='100%' height='1000'></iframe>
-      <iframe src='https://widgets.sociablekit.com/facebook-page-posts/iframe/25379957' frameborder='0' width='100%' height='1000'></iframe>
-      <ul>
-        {posts.map((post, index) => (
-          <li key={index}>
-            <p>{post.content}</p>
-            <p>Sentiment: {post.sentiment}</p>
-            <ul>
-              {post.comments && Array.isArray(post.comments) ? (
-                post.comments.map((comment, commentIndex) => (
-                  <li key={commentIndex}>{comment}</li>
-                ))
-              ) : (
-                <li>No comments available</li>
-              )}
-            </ul>
-          </li>
-        ))}
-      </ul>
+    <div className='App-header'>
+        <p className='icon'>{/* {result?.score === 0 ? '😐' : result?.score < 0 ? '😞' :'🙂'} */} 🙂😐😞</p>
+        <input type="text" value={inputText} onChange={(e)=>setInputText(e.target.value)} placeholder='Enter text here' />
+        <div className='result'>
+          {
+            result?.negative.map((item,index)=><span className='negative'>{item}</span>)
+          }
+          {
+            result?.positive.map((item,index)=><span className='positive'>{item}</span>)
+          }
+        </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
